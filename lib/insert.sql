b2c1ddf963f3796f5d509095c9679298515f2ef7
nonnie sankara <nonniesankarakenya@gmail.com>
2025-05-21 11:03:30 +0300

    changes

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES(1, 'Mr. Chocolate', 3, 'M', 'Brown', 'Friendly', 1);
+INSERT INTO bears VALUES(2, 'Rowdy', 2, 'M', 'Brown', 'Friendly', 1);
+INSERT INTO bears VALUES(3, 'Tabitha', 5, 'F', 'Black', 'Lazy', 1);
+INSERT INTO bears VALUES(4, 'Seargent Brown', 4, 'M', 'Brown', 'Curious', 1);
+INSERT INTO bears VALUES(5, 'Melissa', 6, 'F', 'Yellow', 'Friendly', 1);
+INSERT INTO bears VALUES(6, 'Grinch', 7, 'M', 'Brown', 'Friendly', 0);
+INSERT INTO bears VALUES(7, 'Wendy', 2, 'F', 'Black', 'Playful', 1);
+INSERT INTO bears VALUES(8, null, 1, 'M', 'Brown', 'Friendly', 0);
